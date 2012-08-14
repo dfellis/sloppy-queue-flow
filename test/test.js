bb36812fe2276e99cdd801ad2d80cc8b1823465f
David Ellis <d.f.ellis@ieee.org>
2012-08-13 18:16:43 -0700

    Initial test (even though I can't run it, yet)

---
+++ test/test.js
@@ -0,0 +1,22 @@
+var q = require('queue-flow');
+var sloppy = require('../lib/sloppy-queue-flow');
+
+exports.sloppy = function(test) {
+	test.expect(3);
+	q([1, 2, 3, 4, 5])
+		.map(function(val) {
+			return val*2;
+		})
+		.toArray(function(outArr) {
+			test.equal([2, 4, 6, 8, 10].toString(), outArr.toString(), 'queue-flow is behaving properly');
+		});
+	q([1, 2, 3, 4, 5], sloppy)
+		.map(function(val, callback) {
+			setTimeout(callback.bind(this, val*2), 1000/val);
+		})
+		.toArray(function(outArr) {
+			test.equal([2, 4, 6, 8, 10].toString(), outArr.sort(function(a,b) { return a-b; }).toString(), 'sloppy-queue-flow passes along all values');
+			test.notEqual([2, 4, 6, 8, 10].toString(), outArr.toString(), 'sloppy-queue-flow passes along results on a first-done basis');
+			test.done();
+		});
+};
