76adfe6c4a2c98b4d4c0d887bcee8d64b55d7d83
David Ellis <d.f.ellis@ieee.org>
2012-10-28 18:17:09 -0700

    Removed 'self' from library and added jscoverage checking to the test suite.

--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
+var jscoverage = require('jscoverage');
+var require = jscoverage.require(module);
 var q = require('queue-flow');
-var sloppy = require('../lib/sloppy-queue-flow');
+var sloppy = require('../lib/sloppy-queue-flow', true);
 
 exports.sloppy = function(test) {
 	test.expect(3);
@@ -19,4 +21,29 @@ exports.sloppy = function(test) {
 			test.equal([2, 4, 6, 8, 10].toString(), outArr.sort(function(a,b) { return a-b; }).toString(), 'sloppy-queue-flow passes along all values');
 			test.done();
 		});
+};
+
+exports.jscoverage = function(test) {
+    test.expect(1);
+    var file, tmp, source, total, touched;
+    for (var i in _$jscoverage) {
+        test.ok(true, 'only one file tested by jscoverage');
+        file = i;
+        tmp = _$jscoverage[i];
+        source = _$jscoverage[i].source;
+        total = touched = 0;
+        for (var n=0,len = tmp.length; n < len ; n++){
+            if (tmp[n] !== undefined) {
+                total ++ ;
+                if (tmp[n] > 0) {
+                    touched ++;
+                } else {
+                    console.log(n + "\t:" + source[n-1]);
+                }
+            }
+        }
+        // test.equal(total, touched, 'All lines of code touched by test suite');
+        // Eventually will uncomment the above line, just like in the main queue-flow repo
+    }
+    test.done();
 };
